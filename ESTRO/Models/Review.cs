ef5a82d290cd503161a4136bd3f022f5eacc601a
sinthi-cse <sadia.haque.sinthi203@gmail.com>
2026-08-07 19:52:00 +0600

    adding the review model

---
+++ ESTRO/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESTRO.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
