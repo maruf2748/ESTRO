5a1313c0b5093311443dddc8c77b288e7115b65e
Rejauna Islam <321788426+rejaunaIslam@users.noreply.github.com>
2026-08-08 10:15:00 +0600

    adding product id to orders

---
+++ ESTRO/Data/Migrations/20260526051705_AddProductIdToOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESTRO.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductIdToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Orders");
+        }
+    }
+}
